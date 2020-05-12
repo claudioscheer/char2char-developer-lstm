42d21878e9eb6df91238e57c906a768fed294f14
Claudio Scheer <claudioscheer@protonmail.com>
2020-05-12 11:34:59 -0300

    Add dropout layer

--- src/lib/model.py
+++ src/lib/model.py
@@ -6,18 +6,21 @@ class LSTMModel(nn.Module):
     def __init__(self, input_size, output_size):
         super(LSTMModel, self).__init__()
 
-        self.n_layers = 2
-        self.hidden_size = 64
+        self.n_layers = 3
+        self.hidden_size = 128
 
         self.lstm = nn.LSTM(
             input_size, self.hidden_size, self.n_layers, batch_first=True, dropout=0.5
         )
+        self.dropout = nn.Dropout(0.5)
         self.fc = nn.Linear(self.hidden_size, output_size)
 
     def forward(self, x, previous_hidden_states):
         output, hidden_states = self.lstm(x, previous_hidden_states)
-        output = output.contiguous().view(-1, self.hidden_size)
 
+        output = self.dropout(output)
+
+        output = output.contiguous().view(-1, self.hidden_size)
         output = self.fc(output)
 
         return output, hidden_states
